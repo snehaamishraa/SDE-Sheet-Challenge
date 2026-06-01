93ebe85074ceac96b3a097415db79f23d7d63123
Sneha Mishra <156604552+snehaamishraa@users.noreply.github.com>
2026-06-01 22:16:06 +0530

    Revise comment for Pascal's Triangle implementation
    Updated the comment to reference LeetCode problem 118.

--- Day-01/Pascal_Triangle.py
+++ Day-01/Pascal_Triangle.py
@@ -1,4 +1,4 @@
-#Given an integer numRows, return the first numRows of Pascal's triangle. In Pascal's triangle, each number is the sum of the two numbers
+# LeetCode 118 - Set Matrix Zeroes
 class Solution:
     def generate(self, numRows):
 
