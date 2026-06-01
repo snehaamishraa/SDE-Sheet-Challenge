59e57396984387ae9f11a535064a1a1bafda78a0
Sneha Mishra <156604552+snehaamishraa@users.noreply.github.com>
2026-06-01 22:14:08 +0530

    Day 1 - 118. Pascal's Triangle
    Implement function to generate Pascal's triangle.

---
+++ Day-01/Pascal_Triangle.py
@@ -0,0 +1,18 @@
+#Given an integer numRows, return the first numRows of Pascal's triangle. In Pascal's triangle, each number is the sum of the two numbers
+class Solution:
+    def generate(self, numRows):
+
+        triangle = []
+
+        for i in range(numRows):
+
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle  
+        
+
